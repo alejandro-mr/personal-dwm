71504fe7026382bbae4f623a5f8a449d63717c5b
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2015-08-23 01:17:59 -0700

    Adding tiling mode for gaplessgrid, enabled when pressing 'alt+g'

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include "gaplessgrid.c"
 
 /* appearance */
 #define NUMCOLORS 5
@@ -75,6 +76,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
     { "TTT",      htile },
+    { "###",      gaplessgrid },
 };
 
 /* key definitions */
@@ -111,6 +113,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_v,      setlayout,      {.v = &layouts[3]} },
+    { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4] } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
